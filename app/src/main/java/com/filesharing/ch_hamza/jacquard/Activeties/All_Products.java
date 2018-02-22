409c39453845d4137414b34f12abbfc37b646b71
HamzaMubashir <hamzamubashir149@gmail.com>
2018-02-22 15:01:29 +0500

    Hamza

--- app/src/main/java/com/filesharing/ch_hamza/jacquard/Activeties/All_Products.java
+++ app/src/main/java/com/filesharing/ch_hamza/jacquard/Activeties/All_Products.java
@@ -14,6 +14,7 @@ public class All_Products extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all__products);
     }
